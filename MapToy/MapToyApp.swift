83363ae579343e255d5f8c841ca7bd9de99b1822
Nathan Reed <nathreed@gmail.com>
2022-10-22 15:00:53 -0400

    Initial Commit

---
+++ MapToy/MapToyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapToyApp.swift
+//  MapToy
+//
+//  Created by Nathan Reed on 10/22/22.
+//
+
+import SwiftUI
+
+@main
+struct MapToyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
